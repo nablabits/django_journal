eafb6bfecc95b91c58ce7f2dc5da65c4b3843346
nablabits <birrick@gmail.com>
2018-03-21 16:27:20 +0100

    rid setup

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#This is an auto setup scrip assuming you have python3, pip and requierments.txt
-python3 -m venv venv && echo "environment created" && 
-source venv/bin/activate && echo "env activated" &&
-pip install --upgrade pip && echo "upgrading pìp" &&
-pip install -r requirements.txt && echo "installed requirements" &&
-echo "Done!"
