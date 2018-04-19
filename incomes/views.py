aae3b4a7b1e2f186c181cf3abf095c34bc7e9cc1
nablabits <birrick@gmail.com>
2018-04-19 16:30:14 +0200

    MonthSummary & MonthDropdown def.
    Created a new class View Utils that wraps all the queries done to the db
    so views keep clean. MonthSummary shows the global month incomes, while
    MonthDropdown is suposed to show the individual incomes per month & per
    customer.

--- incomes/views.py
+++ incomes/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView
 from django.shortcuts import render
-from .models import Income, Hour
+from .models import Income, Hour, Customer
 from datetime import date
 from django.db.models import Sum
 
@@ -13,34 +13,70 @@ class IncomesList(ListView):
 
 
 def MainView(request):
-    """Display several queries to get a more complex view."""
-    def month_str(month):
-        """Create a list with all the values for a single month."""
-        incomes_list = Income.objects.filter(date__month=month)
-        incomes = incomes_list.aggregate(Sum('cash'))
+    """Display several queries to get a more complex view based on tabs."""
+    utils = ViewUtils()
 
-        hours_list = Hour.objects.filter(month=month)
-        hours = hours_list.aggregate(Sum('dedication'))
-
-        incomes = incomes['cash__sum']
-        hours = hours['dedication__sum']
-        if hours == 0 or hours is None:
-            ratio = 'NaN'
-        else:
-            ratio = incomes / hours
-
-        data = (month, incomes, hours, ratio)
-        return data
-
-    last5 = Income.objects.order_by('date').reverse()[:5]
-
-    df = []
+    df, details = [], []
     this_month = date.today().month
     for month in range(1, this_month):
-        data = month_str(month)
-        df.append(data)
+        data_month = utils.MonthSummary(month)
+        data_by_customer = utils.MonthDropDown(month)
+        df.append(data_month)
+        details.append(month, data_by_customer)
+
+    last5 = utils.last5
 
     dict4view = {'df': df,
+                 # 'detail': detail,
                  'last5': last5}
 
     return render(request, 'incomes/complex.html', dict4view)
+
+
+class ViewUtils(object):
+    """Useful queries to use on the views."""
+
+    incomes = Income.objects.filter
+    hours = Hour.objects.filter
+    last5 = Income.objects.order_by('date').reverse()[:5]
+
+    def MonthSummary(self, month):
+        """Create a list with all the values for a single month.
+
+        This list shows the name of the month (str), the hours spent (rouded
+        float), the incomes (rounded float) & the ratio (rounded float).
+        """
+        incomes = self.incomes(date__month=month).aggregate(Sum('cash'))
+        hours = self.hours(month=month).aggregate(Sum('dedication'))
+
+        incomes = round(incomes['cash__sum'], 2)
+        hours = hours['dedication__sum']
+        if hours == 0 or hours is None:
+            hours = 0
+            ratio = 'NaN'
+        else:
+            hours = round(hours)
+            ratio = round(incomes / hours, 2)
+
+        month_name = date(2018, month, 1).strftime('%B')
+
+        data = (month_name, incomes, hours, ratio)
+        return data
+
+    def MonthDropDown(self, month):
+        """Show the month's breakdown when row is clicked.
+
+        Continue here.
+        """
+        incomes = self.incomes(date__month=month)
+        hours = self.hours(month=month)
+        customers = Customer.objects.all()
+        result = []
+        for customer in customers:
+            income = incomes.filter(who=customer.id).aggregate(Sum('cash'))
+            hour = hours.filter(who=customer.id).aggregate(Sum('dedication'))
+            data = (customer.name, income, hour)
+            result.append(data)
+            month_name = date(2018, month, 1).strftime('%B')
+
+        return (result)
