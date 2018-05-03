b972813de40a34d5cc2ae9f1f14abad5b1dd3f75
nablabits <birrick@gmail.com>
2018-05-03 19:48:37 +0200

    Cool admin view for incomes.
    Now incomes are shown as a table that can be filtered at will.

--- incomes/admin.py
+++ incomes/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Income, Hour, Customer
 
-admin.site.register(Income)
 admin.site.register(Hour)
 admin.site.register(Customer)
+
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date'
+    list_display = ('date', 'who', 'cash', 'tip', 'comments')
+    list_filter = ('who', )
